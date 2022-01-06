7778dc9e8387d70014f92d7ead1e126efb820eda
fbara <fbara1@yahoo.com>
2022-01-06 11:35:43 -0600

    Initial Commit

---
+++ TrelloClone/TrelloCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloCloneApp.swift
+//  TrelloClone
+//
+//  Created by Frank Bara on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TrelloCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
