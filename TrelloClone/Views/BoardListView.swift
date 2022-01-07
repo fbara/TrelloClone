949d9d7710f145f53e82141af6163a0390bd6a5e
fbara <fbara1@yahoo.com>
2022-01-07 13:54:02 -0600

    implemented basic UI elements.

--- TrelloClone/Views/BoardListView.swift
+++ TrelloClone/Views/BoardListView.swift
@@ -6,11 +6,14 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct BoardListView: View {
     
     @ObservedObject var board: Board
     @StateObject var boardList: BoardList
+    
+    @State var listHeight: CGFloat = 0
     
     var body: some View {
         
@@ -20,6 +23,7 @@ struct BoardListView: View {
             
             listView
                 .listStyle(.plain)
+                .frame(maxHeight: listHeight)
             
             Button("+ Add Card") {
                 
@@ -67,6 +71,9 @@ struct BoardListView: View {
             .listRowSeparator(.hidden)
             .listRowInsets(.init(top: 4, leading: 8, bottom: 4, trailing: 8))
             .listRowBackground(Color.clear)
+            .introspectTableView {
+                listHeight = $0.contentSize.height
+            }
         }
     }
 }
@@ -75,7 +82,7 @@ struct BoardListView_Previews: PreviewProvider {
     @StateObject static var board = Board.stub
     
     static var previews: some View {
-        BoardListView(board: board, boardList: board.lists[0])
+        BoardListView(board: board, boardList: board.lists[0], listHeight: 512)
             .previewLayout(.sizeThatFits)
             .frame(width: 300, height: 512)
     }
