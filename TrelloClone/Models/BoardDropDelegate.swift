04dfc6146bee944887066951f45c51239f3c84a9
fbara <fbara1@yahoo.com>
2022-01-17 11:07:00 -0600

    Implemented Board List reordering.

--- TrelloClone/Models/BoardDropDelegate.swift
+++ TrelloClone/Models/BoardDropDelegate.swift
@@ -13,14 +13,36 @@ struct BoardDropDelegate: DropDelegate {
     let board: Board
     let boardList: BoardList
     
+    @Binding var lists: [BoardList]
+    @Binding var current: BoardList?
+    
+    private func boardListItemProviders(info: DropInfo) -> [NSItemProvider] {
+        info.itemProviders(for: [BoardList.typeIdentifier])
+    }
+    
     private func cardItemProviders(info: DropInfo) -> [NSItemProvider] {
         info.itemProviders(for: [Card.typeIdentifier])
+    }
+    
+    func dropEntered(info: DropInfo) {
+        guard
+            !boardListItemProviders(info: info).isEmpty,
+            let current = current,
+            boardList != current,
+            let fromIndex = lists.firstIndex(of: current),
+            let toIndex = lists.firstIndex(of: boardList) else {
+                return
+            }
+        
+        lists.move(fromOffsets: IndexSet(integer: fromIndex), toOffset: toIndex > fromIndex ? toIndex + 1 : toIndex)
     }
     
     func dropUpdated(info: DropInfo) -> DropProposal? {
         // if the card is not empty
         if !cardItemProviders(info: info).isEmpty {
             return DropProposal(operation: .copy)
+        } else if !boardListItemProviders(info: info).isEmpty {
+            return DropProposal(operation: .move)
         }
         return nil
     }
@@ -38,6 +60,7 @@ struct BoardDropDelegate: DropDelegate {
                 }
             }
         }
+        self.current = nil
         
         return true
     }
